296be52878a97d315614c98862b66ea15470968e
lutzzdias <thiagolutzdias@hotmail.com>
2024-02-26 04:57:30 +0000

    refactor: basic refactoring of date, more to come

--- lua/calendrum/date.lua
+++ lua/calendrum/date.lua
@@ -1,48 +1,50 @@
+--- @class Date
 local M = {}
+M.__index = M
+
+function M.new()
+	-- create new instance with default values
+	local instance = {}
+
+	-- anything not found on instance will be looked up in M, including functions
+	setmetatable(instance, M)
+
+	return instance
+end
 
 M.week_days = {
-	{
-		name = "Sunday",
-		abbreviation = "S",
-	},
-	{
-		name = "Monday",
-		abbreviation = "M",
-	},
-	{
-		name = "Tuesday",
-		abbreviation = "T",
-	},
-	{
-		name = "Wednesday",
-		abbreviation = "W",
-	},
-	{
-		name = "Thursday",
-		abbreviation = "T",
-	},
-	{
-		name = "Friday",
-		abbreviation = "F",
-	},
-	{
-		name = "Saturday",
-		abbreviation = "S",
-	},
+	{ name = "Sunday", abbreviation = "S" },
+	{ name = "Monday", abbreviation = "M" },
+	{ name = "Tuesday", abbreviation = "T" },
+	{ name = "Wednesday", abbreviation = "W" },
+	{ name = "Thursday", abbreviation = "T" },
+	{ name = "Friday", abbreviation = "F" },
+	{ name = "Saturday", abbreviation = "S" },
 }
 
+--- @param year number
+--- @return boolean
 M.is_leap_year = function(year)
 	-- if year is divisible by 4 it is a leap year (with exceptions)
 	-- exceptions: if year is divisible by 100 and by 400 it is a leap year
 	return (year % 4 == 0) and not (year % 100 == 0) or (year % 400 == 0)
 end
 
+--- returns the first day of a given month
+--
+--- @param year number
+--- @param month number
+--- @return osdate | string
 M.get_first_day = function(year, month)
-	-- time = first day of the month
 	local time = os.time({ year = year, month = month, day = 1 })
 	return os.date("*t", time)
 end
 
+--- returns the last day of the previous month of a given month
+--
+--- @param year number
+--- @param month number
+--- @return osdate | string
 M.get_last_day = function(year, month)
 	local next_month_year = year
 	local next_month = month + 1
@@ -57,6 +59,9 @@ M.get_last_day = function(year, month)
 	return os.date("*t", time)
 end
 
+--- @param year number
+--- @param month number
+--- @return string[]
 M.build_month = function(year, month)
 	local calendar = {}
 	local empty_cell = "  "
@@ -66,25 +71,28 @@ M.build_month = function(year, month)
 	local last_day = M.get_last_day(year, month).day
 
 	-- insert week days abbreviations
-	calendar[1] = {}
+	local week_days = ""
 	for i = 1, 7 do
-		calendar[1][i] = " " .. M.week_days[i].abbreviation
+		week_days = week_days .. " " .. M.week_days[i].abbreviation .. " "
 	end
+	calendar[1] = week_days
 
 	-- insert days
 	for i = 2, 7 do
-		calendar[i] = {}
+		local week = {}
 		for j = 1, 7 do
 			-- empty -> first week and before start of current month
 			if i == 2 and j < first_day then
-				calendar[i][j] = empty_cell
+				week[j] = empty_cell
 
 			-- always 2 digits
 			elseif day <= last_day then
-				calendar[i][j] = string.format("%2d", day)
+				week[j] = string.format("%2d", day)
 				day = day + 1
 			end
 		end
+
+		calendar[i] = table.concat(week, " ")
 
 		if day > last_day then
 			break
--- lua/calendrum/init.lua
+++ lua/calendrum/init.lua
@@ -7,11 +7,12 @@ M.setup = function(opts)
 	opts = opts or {}
 	local now = os.date("*t")
 	local current_month = Date.build_month(now.year, now.month)
-	local week_days = table.concat(current_month[1], " ")
 
 	local v = View.new()
 
-	v.text:append(week_days)
+	for _, week in ipairs(current_month) do
+		v.text:append(week)
+	end
 
 	v:create()
 	print(vim.inspect(v))
