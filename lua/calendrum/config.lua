419a7b751e84f5a91b6a4b8befa850313b3fef1d
lutzzdias <thiagolutzdias@hotmail.com>
2024-03-01 02:40:53 +0000

    refactor: delete original config and leave defaults empty

--- lua/calendrum/config.lua
+++ lua/calendrum/config.lua
@@ -3,29 +3,7 @@ local M = {}
 M.namespace = vim.api.nvim_create_namespace("Calendrum")
 
 ---@class Options
-local defaults = {
-	icons = {
-		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-		separator = "➜", -- symbol used between a key and it's label
-		group = "+", -- symbol prepended to a group
-	},
-	window = {
-		border = "none", -- none, single, double, shadow
-		position = "bottom", -- bottom, top
-		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-		padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-		winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
-		zindex = 1000, -- positive value to position WhichKey above other floating windows.
-	},
-	layout = {
-		-- < 1 = percentage of editor size
-		-- > 1 = fixed value
-		size = {
-			width = 0.8,
-			height = 0.8,
-		},
-	},
-}
+local defaults = {}
 
 ---@type Options
 M.options = {}
