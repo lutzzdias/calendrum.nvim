54079ca20fdfe56e2763e63e8cec989be4b9f72b
lutzzdias <thiagolutzdias@hotmail.com>
2024-03-01 02:42:36 +0000

    feat: test all new features

--- lua/calendrum/init.lua
+++ lua/calendrum/init.lua
@@ -1,21 +1,20 @@
-local Date = require("calendrum.date")
 local View = require("calendrum.view")
+local Calendar = require("calendrum.calendar")
 
 local M = {}
 
 M.setup = function(opts)
 	opts = opts or {}
-	local now = os.date("*t")
-	local current_month = Date.build_month(now.year, now.month)
 
 	local v = View.new()
+	local c = Calendar.new()
 
-	for _, week in ipairs(current_month) do
-		v.text:append(week)
+	for _, d in ipairs(c.month.days) do
+		v.text:append(d.date, d.highlight)
 	end
 
 	v:create()
-	print(vim.inspect(v))
+	v:render()
 end
 
 M.setup()
